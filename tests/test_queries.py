56f4c78d8cd92a44c0ba8b40686625aab3d4423e
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-18 15:56:15 -0400

    Update test_queries.py

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -13,7 +13,17 @@ class TestWalkerEvansCatsQueries:
 
     def test_get_walker_evans_cats_returns_one_result(self, db_factory):
         test_data = [
-            (1, 'Cats dancing', 'Walker Evans', 'photograph', '1919-01-01', 'Cats'),
+            {
+                'artwork_id': 1,
+                'object_number': '10.10.10',
+                'title': 'Cats dancing',
+                'artists': 'Walker Evans',
+                'medium': 'Film Negative',
+                'date_created': '1944-01-01',
+                'tags': 'Cats',
+                'department': 'Photographs',
+                'classification': 'Photographs'
+            }
         ]
 
         cursor = db_factory(test_data)
@@ -24,17 +34,30 @@ class TestWalkerEvansCatsQueries:
 
     def test_get_walker_evans_cats_concatenates_artist_names(self, db_factory):
         test_data = [
-            (1, 'Cats dancing', 'Walker Evans | Walter Nevins', 'Film Negative', '1940s', 'Cats')
+            {
+                'artwork_id': 1,
+                'object_number': '10.10.10',
+                'title': 'Cats dancing',
+                'artists': 'Walker Evans | Walter Nevins',
+                'medium': 'Film Negative',
+                'date_created': '1940s',
+                'tags': 'Cats',
+                'department': 'Photographs',
+                'classification': 'Photographs'
+            }
         ]
 
         expected_results = [
             {
                 'id': 1,
+                'Object Number': '10.10.10',
                 'Title': 'Cats dancing',
                 'Artists': 'Walker Evans, Walter Nevins',
                 'Medium': 'Film Negative',
                 'Date Created': '1940s',
                 'Tags': 'Cats',
+                'Department': 'Photographs',
+                'Classification': 'Photographs'
             }
         ]
 
@@ -46,17 +69,65 @@ class TestWalkerEvansCatsQueries:
 
     def test_get_walker_evans_cats_concatenates_tag_names(self, db_factory):
         test_data = [
-            (1, 'Cats and birds dancing', 'Walker Evans', 'Film Negative', '1940s', 'Cats | Birds')
+            {
+                'artwork_id': 1,
+                'object_number': '10.10.10',
+                'title': 'Cats and birds dancing',
+                'artists': 'Walker Evans',
+                'medium': 'Film Negative',
+                'date_created': '1940s',
+                'tags': 'Cats | Birds',
+                'department': 'Photographs',
+                'classification': 'Photographs'
+            }
         ]
 
         expected_results = [
             {
                 'id': 1,
+                'Object Number': '10.10.10',
                 'Title': 'Cats and birds dancing',
                 'Artists': 'Walker Evans',
                 'Medium': 'Film Negative',
                 'Date Created': '1940s',
                 'Tags': 'Cats, Birds',
+                'Department': 'Photographs',
+                'Classification': 'Photographs'
+            }
+        ]
+
+        cursor = db_factory(test_data)
+
+        results = get_walker_evans_cats(cursor)
+
+        assert results == expected_results
+
+    def test_get_walker_evans_cats_concatenates_classification_names(self, db_factory):
+        test_data = [
+            {
+                'artwork_id': 1,
+                'object_number': '10.10.10',
+                'title': 'Cats dancing',
+                'artists': 'Walker Evans',
+                'medium': 'Film Negative',
+                'date_created': '1940s',
+                'tags': 'Cats',
+                'department': 'Photographs',
+                'classification': 'Photographs | Prints'
+            }
+        ]
+
+        expected_results = [
+            {
+                'id': 1,
+                'Object Number': '10.10.10',
+                'Title': 'Cats dancing',
+                'Artists': 'Walker Evans',
+                'Medium': 'Film Negative',
+                'Date Created': '1940s',
+                'Tags': 'Cats',
+                'Department': 'Photographs',
+                'Classification': 'Photographs, Prints'
             }
         ]
 
@@ -67,45 +138,163 @@ class TestWalkerEvansCatsQueries:
         assert results == expected_results
 
     def test_get_walker_evans_cats_returns_expected_results(self, db_factory):
+
         test_data = [
-            (1, 'Cats dancing', 'Walker Evans', 'photograph', '1919-01-01', 'Cats'),
-            (2, 'Dogs dancing', 'Walker Evans', 'photograph', '1919-01-01', 'Dogs'),
-            (3, 'Cats and birds dancing', 'Walker Evans', 'photograph', '1919-01-01', 'Cats|Birds'),
-            (4, 'Dogs and birds dancing', 'Walker Evans', 'photograph', '1919-01-01', 'Dogs|Birds'),
-            (5, 'Cats dancing', 'Walter Evans', 'photograph', '1919-01-01', 'Cats'),
-            (6, 'Dogs dancing', 'Walter Evans', 'photograph', '1919-01-01', 'Dogs'),
-            (7, 'Cats and birds dancing', 'Walter Evans', 'photograph', '1919-01-01', 'Cats|Birds'),
-            (8, 'Dogs and birds dancing', 'Walter Evans', 'photograph', '1919-01-01', 'Dogs|Birds'),
-            (9, '[2 Portraits of Jane Smith Evans Painting and 16 Portraits of a Cat, Possibly "The Boss"]',
-             'Walker Evans', 'Film Negative', '1944', 'Cats'),
-            (10, '', 'Walter Nevins|Walter Nevins', 'Film Negative', '', 'Birds|Birds')
+            {
+                "artwork_id": 1,
+                "object_number": "10.10.10",
+                "title": "Cats dancing",
+                "artists": "Walker Evans",
+                "medium": "Film Negative",
+                "date_created": "1919-01-01",
+                "tags": "Cats",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 2,
+                "object_number": "10.10.11",
+                "title": "Dogs dancing",
+                "artists": "Walker Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Dogs",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 3,
+                "object_number": "10.10.12",
+                "title": "Cats and birds dancing",
+                "artists": "Walker Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Cats|Birds",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 4,
+                "object_number": "10.10.13",
+                "title": "Dogs and birds dancing",
+                "artists": "Walker Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Dogs|Birds",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 5,
+                "object_number": "10.10.14",
+                "title": "Cats dancing",
+                "artists": "Walter Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Cats",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 6,
+                "object_number": "10.10.15",
+                "title": "Dogs dancing",
+                "artists": "Walter Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Dogs",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 7,
+                "object_number": "10.10.16",
+                "title": "Cats and birds dancing",
+                "artists": "Walter Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Cats|Birds",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 8,
+                "object_number": "10.10.17",
+                "title": "Dogs and birds dancing",
+                "artists": "Walter Evans",
+                "medium": "photograph",
+                "date_created": "1919-01-01",
+                "tags": "Dogs|Birds",
+                "department": "Photographs",
+                "classification": "Photographs"
+            },
+
+            {
+                "artwork_id": 9,
+                "object_number": "10.10.18",
+                "title": """[2 Portraits of Jane Smith Evans Painting and 16 Portraits of a Cat, Possibly "The Boss"]""",
+                "artists": "Walker Evans",
+                "medium": "Film Negative",
+                "date_created": "1944-01-01",
+                "tags": "Cats",
+                "department": "Photographs",
+                "classification": "Photographs | Paintings"
+            },
+
+            {
+                "artwork_id": 10,
+                "object_number": "10.10.19",
+                "title": "",
+                "artists": "Walter Nevins|Walter Nevins",
+                "medium": "Film Negative",
+                "date_created": "",
+                "tags": "Birds|Birds",
+                "department": "Photographs",
+                "classification": "Photographs"
+            }
         ]
 
         expected_results = [
             {
                 'id': 1,
+                'Object Number': '10.10.10',
                 'Title': 'Cats dancing',
                 'Artists': 'Walker Evans',
-                'Medium': 'photograph',
+                'Medium': 'Film Negative',
                 'Date Created': '1919-01-01',
                 'Tags': 'Cats',
+                'Department': 'Photographs',
+                'Classification': 'Photographs'
             },
             {
                 'id': 3,
+                'Object Number': '10.10.12',
                 'Title': 'Cats and birds dancing',
                 'Artists': 'Walker Evans',
                 'Medium': 'photograph',
                 'Date Created': '1919-01-01',
                 'Tags': 'Cats, Birds',
+                'Department': 'Photographs',
+                'Classification': 'Photographs'
             },
             {
                 'id': 9,
+                'Object Number': '10.10.18',
                 'Title': '[2 Portraits of Jane Smith Evans Painting and 16 Portraits of a Cat, '
                          'Possibly "The Boss"]',
                 'Artists': 'Walker Evans',
                 'Medium': 'Film Negative',
-                'Date Created': 1944,
+                'Date Created': '1944-01-01',
                 'Tags': 'Cats',
+                'Department': 'Photographs',
+                'Classification': 'Photographs, Paintings'
             }
         ]
 
