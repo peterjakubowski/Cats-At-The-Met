52fbceff13dcf7a3009f64a7f8dbb80eb97d64eb
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-18 15:54:39 -0400

    Update connection.py

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3 import Connection
 from src.database.schema import create_database_schema
 from src.etl.loader import import_artworks_from_csv
 from src.database.queries import get_walker_evans_cats
@@ -11,14 +12,14 @@ PROJECT_ROOT = CURRENT_FILE.parent.parent.parent
 CSV_PATH = PROJECT_ROOT / 'data' / 'MetObjects.csv'
 
 
-def get_db_connection():
+def get_db_connection() -> Connection:
 
     conn = sqlite3.connect(':memory:')
     conn.row_factory = sqlite3.Row
     return conn
 
 
-def fetch_walker_evans_cats_from_db():
+def fetch_walker_evans_cats_from_db() -> list[dict]:
 
     with get_db_connection() as conn:
         cursor = conn.cursor()
