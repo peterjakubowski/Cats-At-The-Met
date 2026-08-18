83db34b8a74751a421af84d93ec42b78e61c98f2
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-18 15:55:26 -0400

    Update loader.py

--- src/etl/loader.py
+++ src/etl/loader.py
@@ -1,14 +1,26 @@
 import csv
+from sqlite3 import Cursor
+from pathlib import Path
 
 
-def load_artworks_data(cursor, raw_data):
+def load_artworks_data(cursor: Cursor, raw_data: list[dict]):
 
     for row in raw_data:
-        artwork_id, title, artist_string, medium, date_created, tags = row
+
+        artwork_id = row.get('artwork_id')
+        object_number = row.get('object_number')
+        title = row.get('title')
+        artist_string = row.get('artists')
+        medium = row.get('medium')
+        date_created = row.get('date_created')
+        tag_string = row.get('tags')
+        department = row.get('department')
+        classification_string = row.get('classification')
+
         cursor.execute('''
-            INSERT INTO artworks (id, Title, Medium, "Date Created")
-            VALUES (?, ?, ?, ?)
-        ''', (artwork_id, title, medium, date_created))
+            INSERT INTO artworks (id, "Object Number", Title, Medium, "Date Created", Department)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (artwork_id, object_number, title, medium, date_created, department))
 
         artist_names = [name.strip() for name in artist_string.split('|')]
 
@@ -27,7 +39,7 @@ def load_artworks_data(cursor, raw_data):
                 VALUES (?, ?)
             ''', (artwork_id, artist_id))
 
-        tag_names = [name.strip() for name in tags.split('|')]
+        tag_names = [name.strip() for name in tag_string.split('|')]
 
         for tag_name in tag_names:
             cursor.execute('''
@@ -44,23 +56,45 @@ def load_artworks_data(cursor, raw_data):
                 VALUES (?, ?)
             ''', (artwork_id, tag_id))
 
+        classification_names = [name.strip() for name in classification_string.split('|')]
 
-def import_artworks_from_csv(cursor, file_path):
+        for class_name in classification_names:
+            cursor.execute('''
+                INSERT OR IGNORE INTO classification (Name) Values (?)
+            ''', (class_name,))
 
-    with open(file_path, mode='r', encoding='utf-8') as f:
+            cursor.execute('''
+                SELECT id FROM classification WHERE Name = ?
+            ''', (class_name,))
+            class_id = cursor.fetchone()['id']
+
+            cursor.execute('''
+                INSERT OR IGNORE INTO artwork_classification (artwork_id, classification_id)
+                VALUES (?, ?)
+            ''', (artwork_id, class_id))
+
+
+def import_artworks_from_csv(cursor: Cursor, file_path: Path):
+
+    with open(file_path, mode='r', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f)
-        # next(reader)  # skip header row
 
         raw_data = []
 
         for row in reader:
-            # row format: [id, title, artist_string, medium, date_created, tags]
-            artwork_id = int(row['Object ID'])
-            title = row['Title']
-            artists = row['Artist Display Name']
-            medium = row['Medium']
-            date_created = row['Object Date']
-            tags = row['Tags']
-            raw_data.append((artwork_id, title, artists, medium, date_created, tags))
+
+            artwork_object = {
+                "artwork_id": int(row['Object ID']),
+                "object_number": row['Object Number'],
+                "title": row['Title'],
+                "artists": row['Artist Display Name'],
+                "medium": row['Medium'],
+                "date_created": row['Object Date'],
+                "tags": row['Tags'],
+                "department": row['Department'],
+                "classification": row['Classification']
+            }
+
+            raw_data.append(artwork_object)
 
         load_artworks_data(cursor, raw_data)
