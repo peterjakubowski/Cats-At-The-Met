e26615ca2b405ac9031a53323513d2f51e0440bd
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-19 20:13:45 -0400

    Update connection.py

--- src/database/connection.py
+++ src/database/connection.py
@@ -2,7 +2,9 @@ import sqlite3
 from sqlite3 import Connection, Cursor
 from src.database.schema import create_database_schema
 from src.etl.loader import import_artworks_from_csv
-from src.database.queries import get_walker_evans_cats
+from src.database.queries import (get_walker_evans_cats,
+                                  count_number_of_cat_artworks_by_artist_name,
+                                  count_number_of_cat_artworks_by_classification)
 from pathlib import Path
 
 CURRENT_FILE = Path(__file__).resolve()
@@ -43,3 +45,31 @@ def fetch_walker_evans_cats_from_db() -> list[dict]:
             import_artworks_from_csv(cursor, CSV_PATH)
 
         return get_walker_evans_cats(cursor)
+
+
+def fetch_number_of_cat_artworks_by_artist_name_from_db() -> list[dict]:
+
+    with get_db_connection(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        if not is_db_initialized(cursor):
+
+            create_database_schema(cursor)
+
+            import_artworks_from_csv(cursor, CSV_PATH)
+
+        return count_number_of_cat_artworks_by_artist_name(cursor)
+
+
+def fetch_number_of_cat_artworks_by_classification_from_db() -> list[dict]:
+
+    with get_db_connection(DB_PATH) as conn:
+        cursor = conn.cursor()
+
+        if not is_db_initialized(cursor):
+
+            create_database_schema(cursor)
+
+            import_artworks_from_csv(cursor, CSV_PATH)
+
+        return count_number_of_cat_artworks_by_classification(cursor)
