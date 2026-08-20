42c04720ee007c94824052278c6983768a83725f
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-19 20:14:27 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from src.database.connection import fetch_walker_evans_cats_from_db
+from src.database.connection import (fetch_walker_evans_cats_from_db,
+                                     fetch_number_of_cat_artworks_by_artist_name_from_db,
+                                     fetch_number_of_cat_artworks_by_classification_from_db)
 import pprint
 
 
@@ -6,9 +8,29 @@ def main():
 
     walker_evans_cats = fetch_walker_evans_cats_from_db()
 
-    print(f"Found {len(walker_evans_cats)} results.")
+    print("==========================================================\n"
+          f"==== Found {len(walker_evans_cats)} artworks by Walker Evans featuring cats. ====\n"
+          "==========================================================\n")
 
     pprint.pprint(walker_evans_cats)
+
+    number_of_cat_artworks_by_artist = fetch_number_of_cat_artworks_by_artist_name_from_db()
+
+    print("\n\n\n"
+          "==========================================\n"
+          "==== Number of Cat Artworks by Artist ====\n"
+          "==========================================\n")
+
+    pprint.pprint(number_of_cat_artworks_by_artist)
+
+    number_of_cat_artworks_by_classification = fetch_number_of_cat_artworks_by_classification_from_db()
+
+    print("\n\n\n"
+          "==================================================\n"
+          "==== Number of Cat Artworks by Classification ====\n"
+          "==================================================\n")
+
+    pprint.pprint(number_of_cat_artworks_by_classification)
 
 
 if __name__ == "__main__":
